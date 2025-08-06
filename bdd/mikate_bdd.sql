935e7b497d828449c57b9eeb2d9f38222d7af5d3
Hyst0s <kr.richard44@gmail.com>
2025-08-06 14:18:30 +0400

    modif3

--- bdd/mikate_bdd.sql
+++ bdd/mikate_bdd.sql
@@ -4,27 +4,27 @@ USE mikate_bdd;
 -- Table utilisateurs
 CREATE TABLE utilisateur 
 (
-    id_utilisateur INT NOT NULL AUTO_INCREMENT PRIMARY KEY;
+    id_utilisateur INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     nom VARCHAR(25) NOT NULL,
     prenom VARCHAR (25) NOT NULL,
     pseudo VARCHAR(25) NOT NULL UNIQUE,
     mot_de_passe VARCHAR(25) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     avatar VARCHAR NOT NULL,
-    role ENUM ('utilisateur', 'admin') DEFAULT 'utilisateur',
+    role ENUM ('utilisateur', 'admin') DEFAULT 'utilisateur'
 );
 
 -- Table Produit
 
 CREATE TABLE produit
 (
-id_produit INT NOT NULL AUTO_INCREMENT PRIMARY KEY;
+id_produit INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 nom VARCHAR(50) NOT NULL,
 description VARCHAR(255) NOT NULL,
 prix DECIMAL NOT NULL,
 image VARCHAR NOT NULL,
 allergene VARCHAR(255) NOT NULL,
-ingredients VARCHAR(255) NOT NULL,
+ingredients VARCHAR(255) NOT NULL
 );
 
 -- Table Panier
@@ -33,7 +33,7 @@ CREATE TABLE panier
 (
     id_panier INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     quantite DECIMAL NOT NULL,
-    id_produit INT NOT NULL,
+    id_produit INT NOT NULL
 );
 
 -- Table commande
@@ -46,7 +46,7 @@ CREATE TABLE commande
     statut VARCHAR(100) NOT NULL,
     adress_livraison VARCHAR(255) NOT NULL,
     date_livraison TIMESTAMP NOT NULL,
-    id_panier INT NOT NULL,
+    id_panier INT NOT NULL
 );
 
 -- Table paiement
@@ -57,5 +57,5 @@ CREATE TABLE paiement
     statut VARCHAR() NOT NULL,
     montant DECIMAL NOT NULL,
     id_transaction INT NOT NULL,
-    date_paiement TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    date_paiement TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
